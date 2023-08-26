9ca7a8c4a7bd70bc8d3b16a546c5b12d114bb02d
oriduruMaho <oridurumai@gmail.com>
2023-08-26 12:24:05 +0900

    一時的コミット

--- calender-front/src/pages/index.tsx
+++ calender-front/src/pages/index.tsx
@@ -1,46 +1,64 @@
-import Image from 'next/image'
-import { Inter, Turret_Road } from 'next/font/google'
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import FullCalendar from '@fullcalendar/react';
-import interactionPlugin from "@fullcalendar/interaction";
-import jaLocale from "@fullcalendar/core/locales/ja";
-import { DayGridView } from '@fullcalendar/daygrid/internal.js';
+// import Image from 'next/image'
+// import { Inter, Turret_Road } from 'next/font/google'
+// import {WeeklyCalendar} from './weeklyCalendar'
+// // // import { Calendar } from '@fullcalendar/core';
+// // // import dayGridPlugin from '@fullcalendar/daygrid';
+// // // import FullCalendar from '@fullcalendar/react';
+// // // import interactionPlugin from "@fullcalendar/interaction";
+// // // import jaLocale from "@fullcalendar/core/locales/ja";
+// // // import { DayGridView } from '@fullcalendar/daygrid/internal.js';
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   let calendarEl: HTMLElement = document.getElementById('calendar')!;
 
-//   let calendar = new Calendar(calendarEl, {
-//     plugins: [ dayGridPlugin ]
-//     // options here
-//   });
+// // // document.addEventListener('DOMContentLoaded', function() {
+// // //   let calendarEl: HTMLElement = document.getElementById('calendar')!;
 
-//   calendar.render();
-// });
+// // //   let calendar = new Calendar(calendarEl, {
+// // //     plugins: [ dayGridPlugin ]
+// // //     // options here
+// // //   });
 
-const inter = Inter({ subsets: ['latin'] })
+// // //   calendar.render();
+// // // });
+
+// // const inter = Inter({ subsets: ['latin'] })
 
 
-export default function Hoge() {
 
-  return (
-    <div>
-      <FullCalendar
+// export default function Hoge() {
 
-        plugins={[
-          dayGridPlugin,
-          interactionPlugin,
-        ]}
-        initialView='dayGridWeek'
-        nowIndicator={true}
-        editable={true}
-        selectable={true}
-        selectMirror={true}
-        initialEvents={[{ title: "initial event", start: new Date() }]}
+//   return (
+//     WeeklyCalendar
+//     <div>
+//       {/* <FullCalendar
 
-      />
-    </div>
-  );
-}
+//         plugins={[
+//           dayGridPlugin,
+//           interactionPlugin,
+//         ]}
+        
+//         initialView='dayGridWeek'
+//         nowIndicator={true}
+//         editable={true}
+//         selectable={true}
+//         selectMirror={true}
+//         initialEvents={[{ title: "initial event", start: new Date() }]}
+//         slotMinTime={9}
+//       /> */}
+//     </div>
+//   );
+// }
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
