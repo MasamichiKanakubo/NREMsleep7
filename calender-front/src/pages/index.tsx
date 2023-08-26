9c5696259e72f588a4679f86ceaa725b9154d193
oriduruMaho <oridurumai@gmail.com>
2023-08-26 11:22:45 +0900

    create calendar

--- calender-front/src/pages/index.tsx
+++ calender-front/src/pages/index.tsx
@@ -1,11 +1,46 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter, Turret_Road } from 'next/font/google'
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import interactionPlugin from "@fullcalendar/interaction";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import { DayGridView } from '@fullcalendar/daygrid/internal.js';
+
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   let calendarEl: HTMLElement = document.getElementById('calendar')!;
+
+//   let calendar = new Calendar(calendarEl, {
+//     plugins: [ dayGridPlugin ]
+//     // options here
+//   });
+
+//   calendar.render();
+// });
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+
+export default function Hoge() {
+
   return (
-   <>
-   <h1>here is honban</h1></>
-  )
+    <div>
+      <FullCalendar
+
+        plugins={[
+          dayGridPlugin,
+          interactionPlugin,
+        ]}
+        initialView='dayGridWeek'
+        nowIndicator={true}
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        initialEvents={[{ title: "initial event", start: new Date() }]}
+
+      />
+    </div>
+  );
 }
