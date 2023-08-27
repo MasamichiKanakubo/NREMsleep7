1a26fb774b0cb425990e44fdb3be66627eff0109
Tetsu-is <hap30305@kwansei.ac.jp>
2023-08-27 18:05:46 +0900

    type def TIme

--- calender-front/src/pages/admin.tsx
+++ calender-front/src/pages/admin.tsx
@@ -4,6 +4,14 @@ export type AdminProps = {
   title: string;
 };
 
+type Time = {
+  acountName: string;
+  acountNumber: number;
+  day: number;
+  timearray: number[];
+  key: number;
+}
+
 const Admin: FC<AdminProps> = () => {
   const axios = require("axios");
   const urlGeneral = `${process.env.NEXT_PUBLIC_API_BASE}/general/`;
@@ -26,19 +34,43 @@ const Admin: FC<AdminProps> = () => {
     }
     );
 
+  axios
+    .get(urlTime)
+    .then((response) => {
+      setTimeArray(response.data);
+      console.log(response);
+      console.log(response.data);
+    }
+    )
+    .catch((error) => {
+      console.log(error);
+    }
+    );
+
   return (
     <>
       <section className="acounts">
         <h1 className="text-blue-400">check acounts</h1>
       </section>
       <section className="generalSetting">
-        <h1>general setting</h1>
+        <h1 className="text-blue-400">general setting</h1>
         <a>deadline:{deadline}</a>
         <br/>
         <a>requiredtime:{requiredtime}</a>
       </section>
       <section className="time">
-        <h1>time setting</h1>
+        <h1 className="text-blue-400">time setting</h1>
+        {timeArray.map((time:Time) => (
+          <div key={time.key}>
+            <a>acountName:{time.acountName}</a>
+            <br/>
+            <a>acountNumber:{time.acountNumber}</a>
+            <br/>
+            <a>day:{time.day}</a>
+            <br/>
+            <a>timearray:{time.timearray}</a>
+          </div>
+        ))}
       </section>
     </>
   );
