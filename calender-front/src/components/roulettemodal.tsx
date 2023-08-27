47abf1f5d77aa0b65560f838861467706fb01902
Kawasaki Yuichi <yuyux0910@gmail.com>
2023-08-28 04:43:50 +0900

    ルーレットで時間が変わるように追加

--- calender-front/src/components/roulettemodal.tsx
+++ calender-front/src/components/roulettemodal.tsx
@@ -1,21 +1,21 @@
+import React, { useState } from "react";
 import Roulette from "@/components/roulette";
-import React from "react";
 
-interface RouletteModelProps {
+interface RouletteModalProps {
     closeModal: () => void;
 }
-export const RouletteModal: React.FC<RouletteModelProps> = ({ closeModal }) => {
+
+export const RouletteModal: React.FC<RouletteModalProps> = ({ closeModal }) => {
+
     return (
         <div className="fixed z-10 top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-80">
-            <div　className="flex flex-col items-center justify-center">
-                <article className="bg-white">
-                    <div className="w-full h-full flex flex-col justify-center items-center">
-                        <Roulette />
-                    </div>
-                    <button className="mt-4 bg-gray-500 text-white px-3 py-1 rounded" onClick={closeModal}>
-                        閉じる
-                    </button>
-                </article>
+            <div className="bg-white rounded-md mt-60 m-auto pt-5 p-10">
+                <div className="w-full h-full flex flex-col justify-center items-center">
+                    <Roulette  />
+                </div>
+                <button className="mt-4 bg-gray-500 text-white px-3 py-1 rounded" onClick={closeModal}>
+                    閉じる
+                </button>
             </div>
         </div>
     );
