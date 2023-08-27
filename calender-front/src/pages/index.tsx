d3dac3cfd67494e452de191b27bdee9e7180b77d
Kawasaki Yuichi <yuyux0910@gmail.com>
2023-08-27 16:50:30 +0900

    「toggleを押すとルーレットを回すボタンが出る」をindex.tsxに追加

--- calender-front/src/pages/index.tsx
+++ calender-front/src/pages/index.tsx
@@ -1,11 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {useState} from "react";
+import Roulette from "../components/roulette";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const [isShow, setIsShow] = useState(true)
   return (
    <>
-   <h1>here is honban</h1></>
+   <h1>here is honban</h1>
+       <button onClick={()=>setIsShow((prev)=>!prev)}>toggle</button>
+       {
+           isShow && <Roulette />
+       }
+   </>
   )
 }
