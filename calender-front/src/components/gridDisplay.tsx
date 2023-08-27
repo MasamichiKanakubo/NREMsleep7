6bfd80d597c62cb2a3d148ef41a09956ce7dfb77
oriduruMaho <oridurumai@gmail.com>
2023-08-27 19:41:31 +0900

    create schedule display

--- calender-front/src/components/gridDisplay.tsx
+++ calender-front/src/components/gridDisplay.tsx
@@ -3,41 +3,16 @@ import style from "../components/calendarStyle.module.css";
 import { HOUR_LIST, TIME_LIST } from "@/constans/timeList";
 
 export const GridDisplay: FC<TimelineProps> = ({ dayList }) => {
-  // const EmptyCell = (date: { date: string }) => {
-  //   return (
-  //     <>
-  //       {HOUR_LIST.map((hourList, index) => {
-  //         return (
-  //           <div
-  //             key={hourList.hour}
-  //             onClick={() => {
-  //               console.log(date, `${hourList.hour}時`);
-  //             }}
-  //             className={style.empty}
-  //           />
-  //         );
-  //       })}
-  //     </>
-  //   );
-  // };
+  const ex1Array = [0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0];
+  const ex2Array = [1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0];
 
-
-  const timeArray = [0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0];
-  const TimeBox  = () => {
-      return (<>{timeArray.map((time, index) => {
+  
+  const TimeBox: FC<{ times: number[] }> = ({ times }) => {
+    return (<>{times.map((time: number) => {
       return (
         <>
-        <p className="text-lg">xxxx</p>
-        <div className={`w-full h-[100px] block bg-red-500`} key={time}>a</div>        
+          <div className={`w-full h-[25px] block ${time === 1 ? "bg-pink-300" : "bg-white"}`} key={time}>a</div>
         </>
-        
-        // <>{((): any => {
-        //   if (time == 1) {
-        //     return (<div className=" bg-red-600 w-full h-[100px] block" key={time} style={{ gridColumn: index + 1 }}></div>);
-        //   } else {
-        //     return (<div className=" bg-blue-600 w-full h-[100px] block" key={time} style={{ gridColumn: index + 1 }}></div>);
-        //   }
-        // })()}</>
       );
     })}</>);
   };
@@ -61,11 +36,7 @@ export const GridDisplay: FC<TimelineProps> = ({ dayList }) => {
                 style={{ gridRow: index + 1 }}
                 className={style.calendarColumn}
               >
-                  
-                    <TimeBox />
-                    <h1 className="bg-slate-300">llll</h1>
-
-                {/* <EmptyCell date={dayItem.date} /> */}
+                <TimeBox times={ex1Array} />
               </div>
             );
           })}
