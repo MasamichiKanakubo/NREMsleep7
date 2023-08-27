89ddb35daea5bc055e6e07bbc49814d926a22cc6
oriduruMaho <oridurumai@gmail.com>
2023-08-27 14:45:11 +0900

    modify modal

--- calender-front/src/components/modal.tsx
+++ calender-front/src/components/modal.tsx
@@ -15,10 +15,41 @@ const Modal:FC<ModalProps> = ({ modalOpen, handleClose, handleSubmit, modalInput
   }
 
   return (
-    <div className="block fixed z-10 pt-100 l-0 t-0 w-full h-full overflow-auto bg-white bg-opacity-40">
+    <div className="block fixed z-10 pt-100 l-0 t-0 w-full h-full overflow-auto bg-brack bg-opacity-40">
       <div className="bg-white rounded-md mt-60 m-auto pt-5 p-10 b-1 h-10 w-3/5 ">
         <button className="text-gray-400 float-right text-3xl font-bold hover:text-black focus:text-black" onClick={handleClose}>&times;</button>
-        <input className="w-80 border border-gray-500 mr-5" type="text" ref={modalInputRef} placeholder="小タスクの名前" />
+        <label htmlFor="date">曜日 </label>
+        <input type="date" name="曜日" list="day-of-week-list"/>
+        <datalist id="day-of-week-list">
+          <option value="日"></option>
+          <option value="月"></option>
+          <option value="火"></option>
+          <option value="水"></option>
+          <option value="木"></option>
+          <option value="金"></option>
+          <option value="土"></option>
+        </datalist>
+        <label htmlFor="time"> 開始時刻 </label>
+        <input type="time" name="開始時刻" className="rounded-sm p-1" min="9:00" max="22:00" step="3600" list="data-list" />
+        <label htmlFor="time"> 終了時刻 </label>
+        <input type="time" name="終了時刻" className="rounded-sm p-5" min="9:00" max="22:00" step="3600" list="data-list" />
+        <datalist id="data-list">
+          <option value="09:00"></option>
+          <option value="10:00"></option>
+          <option value="11:00"></option>
+          <option value="12:00"></option>
+          <option value="13:00"></option>
+          <option value="14:00"></option>
+          <option value="15:00"></option>
+          <option value="16:00"></option>
+          <option value="17:00"></option>
+          <option value="18:00"></option>
+          <option value="19:00"></option>
+          <option value="20:00"></option>
+          <option value="21:00"></option>
+          <option value="22:00"></option>
+        </datalist>
+        <input type="text" className='justify-around mr-2 rounded-sm p-1 placeholder:text-gray-500' ref={modalInputRef} placeholder=' 理由' />
         <button className="rounded-md border border-black p-1" onClick={handleSubmit}>Submit</button>
       </div>
     </div>
--- calender-front/src/pages/index.tsx
+++ calender-front/src/pages/index.tsx
@@ -4,12 +4,19 @@ import { useRef, useState, useEffect } from "react";
 import Modal from "../components/modal";
 
 export default function App() {
-  // const sample = {
-  //   starttime:
-  //   endtime:
-  //   reason:
-
-  // }
+  const sample = [{
+    dayOfWeek: "月",
+    startTime: "09:00",
+    endTime: "12:00",
+    reason: "バイト"
+  },
+  {
+    dayOfWeek: "火",
+    startTime: "13:00",
+    endTime: "15:00",
+    reason: "法事"
+  },
+]
   const count = 0;
 
   useEffect(() => {
@@ -37,12 +44,12 @@ export default function App() {
   }
 
   const handleDemo = () => {
-    // setSchedule(sample);
+    setModalOpen(true);
   }
 
   return (
     <div className={style.App}>
-      <WeeklyCalendar /> 
+      <WeeklyCalendar />
       <button className=" p-1" onClick={handleDemo}>予定の追加</button>
       <Modal
         modalOpen={modalOpen}
@@ -51,27 +58,6 @@ export default function App() {
         modalInputRef={modalInputRef}
       />
       <div style={{ padding: '30px' }}>
-        <label htmlFor="time">開始時刻 </label>
-        <input type="time" name="開始時刻" className="rounded-sm " min="9:00" max="22:00" step="3600" list="data-list"/>
-        <label htmlFor="time">終了時刻 </label>
-        <input type="time" name="終了時刻" className="rounded-sm " min="9:00" max="22:00" step="3600" list="data-list"/>
-        <datalist id="data-list">
-          <option value="09:00"></option>
-          <option value="10:00"></option>
-          <option value="11:00"></option>
-          <option value="12:00"></option>
-          <option value="13:00"></option>
-          <option value="14:00"></option>
-          <option value="15:00"></option>
-          <option value="16:00"></option>
-          <option value="17:00"></option>
-          <option value="18:00"></option>
-          <option value="19:00"></option>
-          <option value="20:00"></option>
-          <option value="21:00"></option>
-          <option value="22:00"></option>
-        </datalist>
-        <input type="text" className='rounded-sm p-1 placeholder:text-gray-500' ref={reasonRef} placeholder=' 理由' />
       </div>
     </div>
   );
